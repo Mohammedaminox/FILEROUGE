5d6c81c8237e8dd78905253f789537fe15f6e702
Mohammed Amine <aminebenmade1999@gmial.com>
2024-04-18 09:33:47 +0100

    Add ServiceController.php

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IconClass;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        $services = Service::all();
+        $iconClasses = IconClass::all();
+        return view('service', [
+            'services' => $services,
+            'iconClasses' => $iconClasses
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:services',
+            'icon_class' => ''
+        ]);
+        Service::create($request->all());
+        return redirect()->route('service.index')
+            ->with('success', 'service created successfully.');
+    }
+
+    public function update(Request $request, Service $service)
+    {
+        // $request->validate([
+        //     'name' => 'required|unique:categories,name,' . $service->id,
+        //     'icon_class' => '',
+
+        // ]);
+
+        $service->update($request->all());
+
+        return redirect()->route('service.index')
+            ->with('success', 'service updated successfully');
+    }
+
+    public function destroy(Service $service)
+    {
+        $service->delete();
+
+        return redirect()->route('service.index')
+            ->with('success', 'service deleted successfully');
+    }
+}
