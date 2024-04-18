8ac9d8669aa61f5f12b456682e2fc3b1228e6ce8
Mohammed Amine <aminebenmade1999@gmial.com>
2024-04-18 09:23:01 +0100

    Update RoomController.php with add category_id and service_id as a infos of eatch room

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Room;
+use App\Models\Service;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class RoomController extends Controller
@@ -14,12 +16,27 @@ class RoomController extends Controller
         $user = session('user_id');
         $rooms = Room::all();
         $categories = Category::all();
+        $services = Service::all();
 
 
         return view('rooms', [
             'rooms' => $rooms,
             'user' => $user,
-            'categories' => $categories
+            'categories' => $categories,
+            'services' => $services
+        ]);
+    }
+    public function frontIndex()
+    {
+        $rooms = Room::all();
+        $categories = Category::all();
+        $services = Service::all();
+
+
+        return view('frontOffice.index', [
+            'rooms' => $rooms,
+            'categories' => $categories,
+            'services' => $services
         ]);
     }
 
@@ -27,8 +44,8 @@ class RoomController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'room_number' => 'required|unique:rooms|string|max:255',
-            'image' => 'image|mimes:jpeg,png,jpg,gif',  // Ensure valid URL format
-            'room_type' => 'required|string|max:255|in:simple,double,suite',  // Limit to specific types
+            'image' => 'image|mimes:jpeg,png,jpg,gif',  // Ensure valid image format
+            'room_type' => 'required|string|max:255|in:single,double,suite',  // Limit to specific types
             'floor' => 'required|integer|min:1',  // Minimum floor of 1
             'description' => 'nullable|string|max:1000',  // Reasonable description length limit
             'status' => 'required|string|in:vacant,occupied,under_maintenance',
@@ -44,15 +61,52 @@ class RoomController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
+
         $roomData = $request->all();
 
-        $imageName = null;
-        if ($request->hasFile('image')) {
-            $imageName = $request->file('image')->getClientOriginalName();
-            $request->file('image')->move(public_path('Pback/assets/images'), $imageName);
+        $image = $request->file('image');
+        if ($image) {
+            $uniqueFileName = uniqid() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('Pback/assets/images'), $uniqueFileName);
+
+            $roomData['image'] = $uniqueFileName;
         }
 
         // Create the room record
         $room = Room::create($roomData);
+
+        return redirect('room')->with('success', 'Room stored successfully.');
+    }
+
+    public function update(Request $request, Room $room)
+    {
+        // Update the room attributes
+        $room->fill($request->only(['room_type', 'floor', 'description', 'status', 'price', 'max_occupancy', 'category_id']));
+
+        $image = $request->file('image');
+        if ($image) {
+            $uniqueFileName = uniqid() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('Pback/assets/images'), $uniqueFileName);
+            // Update the image field in $roomData
+            $room->image = $uniqueFileName;
+        }
+
+        $room->save();
+
+        return redirect('room')->with('success', 'Room updated successfully.');
+    }
+
+
+    public function destroy(Room $room)
+    {
+        if ($room->image) {
+            $imagePath = 'Pback/assets/images/' . $room->image;
+            if (Storage::exists($imagePath)) {
+                Storage::delete($imagePath);
+            }
+        }
+        $room->delete();
+        return redirect('room')
+            ->with('success', 'room deleted successfully.');
     }
 }
